872167de08d92bf19304f3d73edd66d075fdf4b5
NikoEduard <nied88218@gmail.com>
2024-02-23 17:36:56 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 15;
+export const KEY = '35e27894-9048-4938-9cf8-630f428a4212';
+export const MODAL_CLOSE_SEC = 2.5;
